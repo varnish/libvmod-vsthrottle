d50c316c35236e7c4f22c8d2f796328162a2088b
Dag Haavi Finstad <daghf@varnish-software.com>
2014-04-02 14:51:36 +0200

    General structure of things.

--- src/vmod_vsthrottle.c
+++ src/vmod_vsthrottle.c
@@ -4,30 +4,60 @@
 #include "vrt.h"
 #include "cache/cache.h"
 
+#include "vtree.h"
+
 #include "vcc_if.h"
 
+struct key {
+	const char *key;
+	VRB_ENTRY(key) tree;
+};
+
+static int
+keycmp(const struct key *k1, const struct key *k2) {
+	return (strcmp(k1->key, k2->key));
+}
+
+VRB_HEAD(tbtree, key);
+VRB_PROTOTYPE_STATIC(tbtree, key, tree, keycmp)
+VRB_GENERATE_STATIC(tbtree, key, tree, keycmp);
+
+/* Represents a token bucket for a specific key. */
+struct tbucket {
+	struct key k;
+	unsigned magic;
+#define TBUCKET_MAGIC 0x53345eb9
+	double last_used;
+	double period;
+	long tokens;
+	long capacity;
+	pthread_mutex_t mtx;
+};
+
+struct vsthrottle {
+	unsigned magic;
+#define VSTHROTTLE_MAGIC 0xc9f28325
+	pthread_mutex_t mtx;
+	struct tbtree tbs;
+};
+
 int
-init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
-{
+init_function(struct vmod_priv *priv, const struct VCL_conf *conf) {
 	return (0);
 }
 
-VCL_STRING
-vmod_hello(const struct vrt_ctx *ctx, VCL_STRING name)
-{
-	char *p;
-	unsigned u, v;
+static struct tbucket *
+get_bucket(const char *key) {
+	return (NULL);
+}
 
-	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
-	p = ctx->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
-	v++;
-	if (v > u) {
-		/* No space, reset and leave */
-		WS_Release(ctx->ws, 0);
-		return (NULL);
-	}
-	/* Update work space with what we've used */
-	WS_Release(ctx->ws, v);
-	return (p);
+static void
+update_tokens(struct tbucket *b) {
+	/* XXX: Assert b->mtx is held. */
+}
+
+VCL_BOOL
+vmod_is_denied(const struct vrt_ctx *ctx, VCL_STRING key, VCL_INT limit,
+    VCL_DURATION period) {
+	return (1);
 }
